5a0cfbaab785eccec44902b029c53b13e2eb6b5a
gohsonghan98 <gohsonghan98@gmail.com>
2020-08-25 18:44:01 +0800

    Add increment: Level-3 (Ability to mark task as done)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Duke {
     private static int textCount;
-    final static int MAX_TASK = 100;
+    private final static int MAX_TASK = 100;
     private static String[] taskList = new String[MAX_TASK];
+    private static int[] taskListStatus = new int[MAX_TASK];
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -17,13 +18,15 @@ public class Duke {
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
-        Duke.commandEcho();
+        Duke.executeCommand();
     }
 
-    public static void commandEcho() {
-        //different response for different types of command
+    // Recognise command passed to Duke
+    public static void executeCommand() {
+        // Execute different response for different types of command
         String line;
         Scanner in;
+        int taskNumber;
         while (true) {
             in = new Scanner(System.in);
             line = in.nextLine();
@@ -32,6 +35,11 @@ public class Duke {
             }
             if (line.equalsIgnoreCase("list")){
                 displayList();
+                continue;
+            }
+            if (line.contains("done")){
+                taskNumber = Integer.parseInt(line.replace("done", " ").trim());
+                setTaskToDone(taskNumber-1);
                 continue;
             }
             storeText(line);
@@ -50,18 +58,34 @@ public class Duke {
         textCount++;
     }
 
-    public static void displayList(){
-        int i = 1;
+    public static void displayList() {
+        int index = 1;
         System.out.println("\t____________________________________________________________");
-        for(String listTask: taskList){
-            if(listTask == null){
+        System.out.println("\tHere are the tasks in your list:");
+        for (String listTask : taskList) {
+            if (listTask == null) {
                 break;
             }
-            System.out.println("\t" + i +". "+ listTask);
-            i++;
+            System.out.println("\t" + index + "." + "[" + getStatusIcon(index) + "] " + listTask);
+            index++;
         }
         System.out.println("\t____________________________________________________________");
     }
 
+    public static String getStatusIcon(int index){
+        if(taskListStatus[index-1] == 1){
+            return "\u2713";
+        } else {
+            return "\u2718";
+        }
+    }
+
+    public static void setTaskToDone(int taskNumber){
+        taskListStatus[taskNumber] = 1;
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t[" + getStatusIcon(1) + "] " + taskList[taskNumber]);
+        System.out.println("\t____________________________________________________________");
+    }
 
 }
