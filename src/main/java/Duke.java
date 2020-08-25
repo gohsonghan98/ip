91817dd7cb55a9fc7c96ed9792286defbbeacef5
gohsonghan98 <gohsonghan98@gmail.com>
2020-08-25 15:24:14 +0800

    Add increment: Level-1 (Greeet, Echo, Exit)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,11 +8,30 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.print("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        Duke.commandEcho();
+    }
+
+    public static void commandEcho () {
+        //different response for different types of command
+        String line;
+        Scanner in;
+        while(true){
+            in = new Scanner(System.in);
+            line = in.nextLine();
+            if(line.equalsIgnoreCase("bye")){
+                break;
+            }
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + line);
+            System.out.println("\t____________________________________________________________");
+        }
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
     }
 }
