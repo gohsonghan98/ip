a6d266fa3b83b3f3ee4784966576b42ca50bb1b2
gohsonghan98 <gohsonghan98@gmail.com>
2020-08-25 17:42:21 +0800

    Add increment: Level-2 (Add, List)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static int textCount;
+    final static int MAX_TASK = 100;
+    private static String[] taskList = new String[MAX_TASK];
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -26,13 +30,38 @@ public class Duke {
             if (line.equalsIgnoreCase("bye")) {
                 break;
             }
-            System.out.println("\t____________________________________________________________");
-            System.out.println("\t" + line);
-            System.out.println("\t____________________________________________________________");
+            if (line.equalsIgnoreCase("list")){
+                displayList();
+                continue;
+            }
+            storeText(line);
         }
 
         System.out.println("\t____________________________________________________________");
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________");
     }
+
+    public static void storeText(String text){
+        taskList[textCount] = text;
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tAdded: " + text);
+        System.out.println("\t____________________________________________________________");
+        textCount++;
+    }
+
+    public static void displayList(){
+        int i = 1;
+        System.out.println("\t____________________________________________________________");
+        for(String listTask: taskList){
+            if(listTask == null){
+                break;
+            }
+            System.out.println("\t" + i +". "+ listTask);
+            i++;
+        }
+        System.out.println("\t____________________________________________________________");
+    }
+
+
 }
