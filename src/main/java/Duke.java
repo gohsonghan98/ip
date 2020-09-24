6f55e5f03c9e70de88169fa3d6c8e75ad95a1727
gohsonghan98 <gohsonghan98@gmail.com>
2020-09-24 20:40:30 +0800

    Add Ui class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,32 +9,30 @@ public class Duke {
     private static int taskCount;
     private static ArrayList<Task> taskList = new ArrayList<>();
     private final static String FILE_PATH = "data/duke.txt";
+    private final static String DIR_PATH = "data/";
     public static File f = new File(FILE_PATH);
+    public static File dir = new File(DIR_PATH);
+    private static Ui ui = new Ui();
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        ui.printWelcome();
 
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello! I'm Duke");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println("\t____________________________________________________________");
         //Load saved tasks from file
         if (f.exists()) {
             try {
                 loadProgress();
             } catch (FileNotFoundException e) {
-                System.out.println("\tFile not found!");
+                ui.printFileNotFound();
             }
         } else {
             try {
+                if (!dir.exists()) {
+                    dir.mkdir();
+                }
                 f.createNewFile();
+                ui.printFileCreated();
             } catch (IOException e) {
-                System.out.println("\tFile not created");
+                ui.printFileNotCreated();
             }
         }
 
@@ -44,7 +42,7 @@ public class Duke {
         try {
             writeToFile(FILE_PATH);
         } catch (IOException e) {
-            System.out.println("\tError saving tasks!");
+            ui.printSaveError();
         }
     }
 
@@ -60,7 +58,7 @@ public class Duke {
                 break;
             }
             if (line.equalsIgnoreCase("list")) {
-                displayList();
+                ui.displayList(taskList);
                 continue;
             }
             if (line.contains("done")) {
@@ -68,7 +66,7 @@ public class Duke {
                 try {
                     taskList.get(taskNumber).setDone();
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("\t ☹ OOPS!!! I'm sorry, but there is no task in that entry :-()");
+                    ui.printNoTask();
                 }
                 continue;
             }
@@ -77,33 +75,28 @@ public class Duke {
                     taskNumber = Integer.parseInt(line.replaceAll("[^\\d]", " ").trim()) - 1;
                     Task taskRemoved = taskList.get(taskNumber);
                     taskList.remove(taskNumber);
-                    System.out.println("\t Noted. I've removed this task:");
-                    System.out.println("\t\t" + "[" + taskRemoved.getTaskSymbol() + "]" + "[" +
-                            taskRemoved.getStatusIcon() + "]" + " " + taskRemoved);
+                    ui.printTaskRemove(taskRemoved);
 
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("\t ☹ OOPS!!! I'm sorry, but there is no task in that entry :-()");
+                    ui.printNoTask();
                 }
                 continue;
             }
             try {
                 storeText(line);
             } catch (DukeException e) {
-                System.out.println("\t ☹ OOPS!!! I'm sorry, but I don't know what that means :-()");
+                ui.printInvalidInput();
             } catch (StringIndexOutOfBoundsException e) {
                 if (line.startsWith("deadline")) {
-                    System.out.println("\t ☹ OOPS!!! The description of a deadline cannot be empty.");
+                    ui.printEmptyDeadline();
                 } else if (line.startsWith("event")) {
-                    System.out.println("\t ☹ OOPS!!! The description of a event cannot be empty.");
+                    ui.printEmptyEvent();
                 } else if (line.startsWith("todo")) {
-                    System.out.println("\t ☹ OOPS!!! The description of a todo cannot be empty.");
+                    ui.printEmptyTodo();
                 }
             }
         }
-
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\t Bye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
+        ui.printByeMessage();
     }
 
     public static void storeText(String text) throws DukeException {
@@ -116,14 +109,14 @@ public class Duke {
             task = text.substring(9, text.indexOf(" \\by"));
             by = text.substring(text.indexOf("\\by") + 4, text.length());
             taskList.add(new Deadline(task, by));
-            printTaskAssignment(taskList.size() - 1);
+            ui.printTaskAssignment(taskList, taskList.size() - 1);
 
         } else if (text.startsWith("event")) {
             //Assign event task
             task = text.substring(6, text.indexOf(" \\at"));
             at = text.substring(text.indexOf("\\at") + 4, text.length());
             taskList.add(new Event(task, at));
-            printTaskAssignment(taskList.size() - 1);
+            ui.printTaskAssignment(taskList, taskList.size() - 1);
 
         } else if (text.startsWith("todo")) {
             //Assign todos task
@@ -132,48 +125,14 @@ public class Duke {
                 throw new StringIndexOutOfBoundsException();
             }
             taskList.add(new Todo(task));
-            printTaskAssignment(taskList.size() - 1);
+            ui.printTaskAssignment(taskList, taskList.size() - 1);
 
         } else {
             throw new DukeException();
         }
     }
 
-    public static void displayList() {
-        int index = 1;
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\t Here are the tasks in your list:");
-        for (Task listTask : taskList) {
-            if (listTask == null) {
-                break;
-            }
-            System.out.println("\t " + index + "." + "[" + listTask.getTaskSymbol() + "]" + "["
-                    + listTask.getStatusIcon() + "] " + listTask);
-            index++;
-        }
-        System.out.println("\t____________________________________________________________");
-    }
-
-    public static void printTaskAssignment(int index) {
-        int taskIndex = index + 1;
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\t Got it. I've added this task:");
-        System.out.println("\t\t" + "[" + taskList.get(index).getTaskSymbol() + "]" + "[" +
-                taskList.get(index).getStatusIcon() + "]" + " " + taskList.get(index));
-        System.out.println("\t Now you have " + taskIndex + " tasks in the list.");
-        System.out.println("\t____________________________________________________________");
-    }
-
-/*    private static void printFileContents(String filePath) throws FileNotFoundException {
-        File f = new File(FILE_PATH);
-        Scanner s = new Scanner(f);
-        while (s.hasNext()) {
-            System.out.println(s.nextLine());
-        }
-    }*/
-
     public static void loadProgress() throws FileNotFoundException {
-        //File contains something
         String textInput;
         Scanner s = new Scanner(f);
         String[] taskArray = new String[4];
---
+++ src/main/java/Ui.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+
+public class Ui {
+    public static final String DIVIDER = ("\t____________________________________________________________");
+    public static final String FILE_NOT_FOUND = ("\t File not found!");
+    public static final String FILE_CREATED = ("\t New file created automatically.");
+    public static final String FILE_NOT_CREATED = ("\t Error! File could not created.");
+    public static final String SAVE_ERROR = ("\t Error saving tasks!");
+    public static final String TASK_LIST_HEADER = ("\t Here are the tasks in your list:");
+    public static final String NO_TASK = ("\t ☹ OOPS!!! I'm sorry, but there is no task in that entry :-()");
+    public static final String INVALID_INPUT = ("\t ☹ OOPS!!! I'm sorry, but I don't know what that means :-()");
+    public static final String EMPTY_DEADLINE = ("\t ☹ OOPS!!! The description of a deadline cannot be empty.");
+    public static final String EMPTY_EVENT = ("\t ☹ OOPS!!! The description of a event cannot be empty.");
+    public static final String EMPTY_TODO = ("\t ☹ OOPS!!! The description of a todo cannot be empty.");
+    public static final String BYE_MESSAGE = ("\t Bye. Hope to see you again soon!");
+    public static final String LOGO = (" ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n");
+
+    public Ui() {
+    }
+
+    public void printWelcome() {
+        System.out.println("Hello from\n" + LOGO);
+        printDivider();
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        printDivider();
+    }
+
+    public void printFileNotFound() {
+        System.out.println(FILE_NOT_FOUND);
+    }
+
+    public void printFileCreated() {
+        System.out.println(FILE_CREATED);
+    }
+
+    public void printFileNotCreated() {
+        System.out.println(FILE_NOT_CREATED);
+    }
+
+    public void printSaveError() {
+        System.out.println(SAVE_ERROR);
+    }
+
+    public void printDivider() {
+        System.out.println(DIVIDER);
+    }
+
+    public void printNoTask() {
+        System.out.println(NO_TASK);
+    }
+
+    public void printInvalidInput() {
+        System.out.println(INVALID_INPUT);
+    }
+
+    public void printEmptyDeadline() {
+        System.out.println(EMPTY_DEADLINE);
+    }
+
+    public void printEmptyEvent() {
+        System.out.println(EMPTY_EVENT);
+    }
+
+    public void printEmptyTodo() {
+        System.out.println(EMPTY_TODO);
+    }
+
+    public void printByeMessage() {
+        printDivider();
+        System.out.println(BYE_MESSAGE);
+        printDivider();
+    }
+
+    public void printTaskRemove(Task taskRemoved) {
+        System.out.println("\t Noted. I've removed this task:");
+        System.out.println("\t\t" + "[" + taskRemoved.getTaskSymbol() + "]" + "[" +
+                taskRemoved.getStatusIcon() + "]" + " " + taskRemoved);
+    }
+
+    public void printTaskListHeader() {
+        System.out.println(TASK_LIST_HEADER);
+    }
+
+    public void displayList(ArrayList<Task> taskList) {
+        int index = 1;
+        printDivider();
+        printTaskListHeader();
+        for (Task listTask : taskList) {
+            if (listTask == null) {
+                break;
+            }
+            System.out.println("\t " + index + "." + "[" + listTask.getTaskSymbol() + "]" + "["
+                    + listTask.getStatusIcon() + "] " + listTask);
+            index++;
+        }
+        printDivider();
+    }
+
+    public void printTaskAssignment(ArrayList<Task> taskList, int index) {
+        int taskIndex = index + 1;
+        printDivider();
+        System.out.println("\t Got it. I've added this task:");
+        System.out.println("\t\t" + "[" + taskList.get(index).getTaskSymbol() + "]" + "[" +
+                taskList.get(index).getStatusIcon() + "]" + " " + taskList.get(index));
+        System.out.println("\t Now you have " + taskIndex + " tasks in the list.");
+        printDivider();
+    }
+}
